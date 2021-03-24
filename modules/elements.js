f364d595a5b9b5e2dfa3fb69188c6ceb54a0218d
voromahery <daniel.her@onja.org>
2021-03-24 14:55:12 +0300

    Removed unecessary files

--- modules/elements.js
+++
@@ -1,5 +0,0 @@
-// Grab all necessary elements and files
-export const table = document.querySelector('tbody');
-export const addButton = document.querySelector('.add-button');
-export const searchName = document.querySelector('.search-name');
-export const searchMonth = document.querySelector('#search-month');
