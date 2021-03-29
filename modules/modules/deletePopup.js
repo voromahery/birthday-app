26907921df205824b6bf66ca48e939b31ddea4b9
voromahery <daniel.her@onja.org>
2021-03-29 08:02:35 +0300

    Removed file

--- modules/modules/deletePopup.js
+++
@@ -1,9 +0,0 @@
-export function removeDeletePopup(container) {
-  container.classList.remove("open");
-
-  // Delete the popup right after
-  container.remove();
-
-  // Remove it from javascript memory
-  container = null;
-}
